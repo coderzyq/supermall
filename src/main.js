54cfad07f55e01daa1c6b0910056ddff30f27571
Albert Zhang <74855025+zhang-yong-qiang@users.noreply.github.com>
2022-02-23 18:22:26 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from "@/store";
 
+import FastClick from 'fastclick'
+import VueLazyload from "vue-lazyload";
+
 import toast from "components/common/toast";
 
 Vue.config.productionTip = false
@@ -10,6 +13,14 @@ Vue.config.productionTip = false
 Vue.prototype.$bus = new Vue()
 //安装toast插件
 Vue.use(toast)
+
+//解决移动端300ms延迟问题
+FastClick.attach(document.body)
+
+//使用懒加载的插件
+Vue.use(VueLazyload, {
+    loading: require('./assets/img/common/placeholder.png')
+})
 
 new Vue({
     render: h => h(App),
