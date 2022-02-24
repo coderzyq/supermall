2e685e33d614c8c2b9c4ef030e4ac788066cfac9
Albert Zhang <74855025+zhang-yong-qiang@users.noreply.github.com>
2022-02-24 09:40:29 +0800

    初始化项目

--- postcss.config.js
+++ postcss.config.js
@@ -6,9 +6,9 @@ module.exports = {
             viewportHeight: 667, //视窗的高度，对应的还我们设计稿的高度（也可以不配置）
             unitPrecision: 5, //指定’px‘转换为视窗单位值的小数位数（很多时候无法整除）
             viewportUnit: 'vw', //指定需要转换成的视窗单位，建议使用vw
-            selectorBlackList: ['ignore', 'tab-bar', 'tab-bar-item'], //指定不需要转换的类
+            selectorBlackList: ['ignore', 'tab-bar', 'tab-bar-item','bottom-bar'], //指定不需要转换的类
             miniPixelValue: 1, //小于或等于’1px‘ 不转换视窗单位。
             mediaQuery: false, //允许在媒体查询中转换’px'
-        },
+        }
     }
 }
